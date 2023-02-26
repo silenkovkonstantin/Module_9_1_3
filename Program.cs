1240c3e04b33a3ca83b0cfb5175c569febc866f1
silenkovkonstantin <silenkov@inbox.ru>
2023-02-26 09:53:01 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace Module_9_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new();
+            exception.Data.Add("Дата создания исключения: ", DateTime.Now);
+        }
+    }
+}
